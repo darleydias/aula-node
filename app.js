7717e4292f0a52ee74b54934fba5aa628cf1608e
darleydias <darlley@gmail.com>
2022-08-11 14:49:19 -0300

    1.6.6

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
+app.use('/static',express.static('public'))
 
-app.get('/', (req, res)=> {
-  res.send('Olá Mundo!');
-});
+//######## CRUD #############
+//######## LISTAR COMARCAS #############
 
-app.get('/opa', (req, res)=> {
-  res.send('Opa Mundo!');
-});
+app.get('/comarcas', (req, res)=> {
+  res.status(200).send('Listando')
+})
+
+//######## BUSCAR COMARCA #############
+
+app.get('/comarcas/:codigo', (req, res)=> {
+  res.status(200).send('Buscar uma Comarca')
+})
+
+//######## INSERIR COMARCA #############
+
+app.post('/comarcas', (req, res)=> {
+  res.status(201).send('Inserindo comarcas')
+})
+
+//######## ATRUALIZAR COMARCA #############
+
+app.put('/comarcas', (req, res)=> {
+  res.status(201).send('Atualizando')
+})
+
+//######## ATUALIZAR COMARCA #############
+app.delete('/comarcas', (req, res)=> {
+  res.status(200).send('Apagando')
+})
 
 app.listen(3000, function() {
   console.log('App de Exemplo escutando na porta 3000!');
-});
+})
