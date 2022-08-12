096a4ddaa3e7f17f9610cfef43fe6979cd6c0cf8
darleydias <darlley@gmail.com>
2022-08-12 20:02:31 -0300

    inicial

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const app = express()
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./swagger_output.json')
 const routeComarcas = require('./routes/comarcas')
+const mongoose = require('mongoose')
 
+const url ='mongodb+srv://darleydias:Catelecom()123@cluster0.vwjrt2z.mongodb.net/?retryWrites=true&w=majority'
+
+const options={
+
+}
+mongoose.connection.on('connected',()=>{
+  console.log('Conectado ao banco mongo corretamente')
+})
+mongoose.connection.on('error',(erro)=>{
+  console.log('erro ao conectar ao mong'+erro)
+})
+mongoose.connect(url,options)
 app.use('/static',express.static('public'))
 app.use(express.json()) // pega o valor do body e transforma em json
 app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument))
